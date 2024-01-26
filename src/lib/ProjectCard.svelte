5b084caef6cf050f1c1486d7aef84713116865b2
Matisse Callewaert <matisse.callewaert@gmail.com>
2024-01-26 10:56:12 +0100

    :art: Update design

--- src/lib/ProjectCard.svelte
+++ src/lib/ProjectCard.svelte
@@ -11,10 +11,32 @@
 	}
 </script>
 
-<div class={colorClass + ' p-6 rounded-xl'}>
-	<div class="{headerColorClass} p-3 rounded-t-xl" style="margin:-1.5rem -1.5rem 0 -1.5rem">
-		<h3 class="text-xl font-semibold">{title}</h3>
-		<p class="text-gray-700">{description}</p>
+<div class={colorClass + ' p-6 rounded-xl card'}>
+	<div class="card-inner">
+		<div class="{headerColorClass} p-3 rounded-t-xl" style="margin:-1.5rem -1.5rem 0 -1.5rem">
+			<h3 class="text-xl font-semibold">{title}</h3>
+			<p class="text-gray-700">{description}</p>
+		</div>
+		<img src={imageUrl} alt={altText} class="mt-4 rounded-xl" />
 	</div>
-	<img src={imageUrl} alt={altText} class="mt-4 rounded-xl" />
 </div>
+
+<style>
+	.card {
+  transition: all 0.2s;
+  position: relative;
+  cursor: pointer;
+}
+
+.card-inner {
+  width: inherit;
+  height: inherit;
+  background: rgba(255,255,255,.05);
+  border-radius: 8px;
+}
+
+.card:hover {
+	transform: scale(1.04) rotate(1deg);
+	transition: transform 0.5s;
+}
+</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,15 +1,3 @@
 <header class="flex justify-between items-center px-8 py-4">
 	<div class="text-gray-700 text-lg font-semibold">MATISSE CALLEWAERT</div>
-	<nav>
-		<ul class="flex space-x-4">
-			<li><a href="/" class="text-gray-700 hover:text-blue-900" sveltekit:prefetch>Home</a></li>
-			<li>
-				<a href="/projects" class="text-gray-700 hover:text-blue-900" sveltekit:prefetch>Projects</a
-				>
-			</li>
-			<li>
-				<a href="/contact" class="text-gray-700 hover:text-blue-900" sveltekit:prefetch>Contact</a>
-			</li>
-		</ul>
-	</nav>
 </header>
