c2336aa7a63f4b630a08237291b40131779c0e44
matissecallewaert <83773311+matissecallewaert@users.noreply.github.com>
2024-01-26 11:51:37 +0000

    Deploying to gh-pages from @ matissecallewaert/portfolio@8bec2955db9de079432c93eaf7e748726ebc9bd4 🚀

--- docs/_app/immutable/entry/start.I8fkxNYF.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.A51aMlT5.js";export{t as start};
--- src/routes/Header.svelte
+++
@@ -1,3 +0,0 @@
-<header class="flex justify-between items-center px-8 py-4">
-	<div class="text-gray-700 text-lg font-semibold">MATISSE CALLEWAERT</div>
-</header>
