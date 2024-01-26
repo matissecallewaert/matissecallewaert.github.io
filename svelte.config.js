1768bf087638ea9b79135c86af270a96cc333532
Matisse Callewaert <matisse.callewaert@gmail.com>
2024-01-26 11:09:21 +0100

    :bug: Fix path

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		adapter: adapter(),
 
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/' : '',
+			base: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
 		  },
 	}
 };
